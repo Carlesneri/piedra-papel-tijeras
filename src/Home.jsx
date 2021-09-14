6c79b483bb909ca6752fb048b39fe26d142a8b9c
Carlesneri <carlesneri@hotmail.com>
2021-09-14 18:56:58 +0200

    feature: start button added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,18 @@
-export default function () {
-    return <h1>Home</h1>
-}
+import React from 'react'
+import './home.css'
+import Hand from './components/Hand/index.jsx'
+import LastHands from './components/LastHands'
+import { IonContent, IonFooter, IonHeader } from '@ionic/react'
+
+export default React.FC = () => 
+    <>
+        <IonHeader>
+            <h1>Home</h1>
+        </IonHeader>
+        <IonContent>
+            <Hand />
+        </IonContent>
+        <IonFooter>
+            <LastHands />
+        </IonFooter>
+    </>
